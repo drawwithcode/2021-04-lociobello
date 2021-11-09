923e46df89789381e3c287419272d5deac410076
lociobello <lorenzo2.bernini@mail.polimi.it>
2021-11-09 19:00:27 +0100

    voglio i diritto di andà alle poste

--- sketch_1.js
+++ sketch_1.js
@@ -1,43 +1,73 @@
+// la frase è scritta al contrario in modo che appaia in ordine giusto quando eseguita
 let phrase =
   "bello. più assai sarebbe ne mondo il e pene meno e pensieri meno sarebbe ci fratello, suo considerasse si ognun e bene di po' un esistesse mondo ni’ Se";
 let words = phrase.split(" ");
 let iterator = 0;
 
 let input1;
-let input2;
-let input3;
-let input4;
 
-function preload() {
-  // pacciani = loadImage("assets/pacciani.mp4");
-}
+let pacciani;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // background("red");
 
-  input1 = createInput("...");
-  input1.addClass("primo");
-  input2 = createInput("...");
-  input2.addClass("secondo");
-  input3 = createInput("...");
-  input3.addClass("terzo");
-  input4 = createInput("...");
-  input4.addClass("quarto");
+  //creazione della casella di testo e assegnazione dell'ID
+  input1 = createElement("textarea");
+  input1.id("primo");
 
+  //creazione dei bottoni per ogni parola della poesia
   words.forEach((word) => {
     createButton(word);
   });
 
   allButtons = selectAll("button");
+
+  //creazione del video
+  pacciani = createVideo(["assets/pacciani_2.mp4"]);
+  pacciani.hide();
 }
 
 function draw() {
-  iterator++;
+  //movimento dei bottoni fluttuanti
+  iterator = iterator + 0.7;
   allButtons.forEach((singleButton, i) => {
     let x = noise((iterator + 30 * i) / 400) * windowWidth;
-    let y = noise((iterator - 10 * i) / 400) * windowHeight;
-
+    let y = noise((iterator - 30 * i) / 400) * windowHeight;
     singleButton.position(x, y);
   });
+
+  //Se il testo scritto nella casella corrisponde, parte il video. Ho inserito varie alterantive (soprattuto per la punteggiatura) in modo da non renderlo troppo frustrante.
+  if (
+    document.getElementById("primo").value ==
+      "Se ni’ mondo esistesse un po’ di bene e ognun si considerasse suo fratello, ci sarebbe meno pensieri e meno pene e il mondo ne sarebbe assai più bello" ||
+    document.getElementById("primo").value ==
+      "Se ni' mondo esistesse un po' di bene e ognun si considerasse suo fratello, ci sarebbe meno pensieri e meno pene e il mondo ne sarebbe assai più bello" ||
+    document.getElementById("primo").value ==
+      "Se ni' mondo esistesse un po' di bene e ognun si considerasse suo fratello ci sarebbe meno pensieri e meno pene e il mondo ne sarebbe assai più bello" ||
+    document.getElementById("primo").value ==
+      "Se ni' mondo esistesse un po' di bene e ognun si considerasse suo fratello ci sarebbe meno pensieri e meno pene e il mondo ne sarebbe assai più bello." ||
+    document.getElementById("primo").value ==
+      "Se ni' mondo esistesse un po' di bene e ognun si considerasse suo fratello, ci sarebbe meno pensieri e meno pene e il mondo ne sarebbe assai più bello." ||
+    document.getElementById("primo").value ==
+      "Se ni' mondo esistesse un po' di bene e ognun si considerasse suo fratello, ci sarebbe meno pensieri e meno pene e il mondo ne sarebbe assai più bello." ||
+    document.getElementById("primo").value ==
+      "se ni' mondo esistesse un po' di bene e ognun si considerasse suo fratello, ci sarebbe meno pensieri e meno pene e il mondo ne sarebbe assai più bello." ||
+    document.getElementById("primo").value ==
+      "se ni' mondo esistesse un po' di bene e ognun si considerasse suo fratello ci sarebbe meno pensieri e meno pene e il mondo ne sarebbe assai più bello"
+  ) {
+    backgroundImage(pacciani);
+    pacciani.play();
+  } else {
+    background(255);
+    pacciani.stop();
+  }
+}
+
+function backgroundImage(img) {
+  push();
+  translate(width / 2, height / 2);
+  imageMode(CENTER);
+  let scale = Math.max(width / img.width, height / img.height);
+  image(img, 0, 0, img.width * scale, img.height * scale);
+  pop();
 }
